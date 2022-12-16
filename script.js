a26e9f3b7344fb8fb2a30fa887a5f16b4fff3a4f
festusniyo <84879082+festusAlb@users.noreply.github.com>
2022-12-16 09:57:18 +0200

    added js

---
+++ script.js
@@ -0,0 +1,67 @@
+var nameError = document.getElementById('name-error');
+var subjectError = document.getElementById('subject-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+
+    if(name.length==0){
+        nameError.innerHTML= 'name is required';
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'write full name';
+        return false;
+    }
+    nameError.innerHTML = '<span>Valid<span>';
+    return true;
+}
+
+function validateSubject(){
+    var subject = document.getElementById('contact-subject').value;
+
+    if(subject.length==0){
+        subjectError.innerHTML= 'subject is required';
+        return false;
+    }
+    subjectError.innerHTML = '<span>Valid<span>';
+    return true;
+}
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length==0){
+        nameError.innerHTML= 'email is required';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'invalid email';
+        return false;
+    }
+    emailError.innerHTML = '<span>Valid<span>';
+    return true;
+}
+
+function validateMessage(){
+    var message = document.getElementById('contact-message').value;
+    var required = 30;
+    var left = required - message.length;
+    if (left>0){
+        messageError.innerHTML = left + 'more characters required';
+        return false;
+    }
+    messageError.innerHTML = '<span>Valid</span>';
+    return true;
+}
+
+function validateForm(){
+    if(!validateName() || !validateSubject() || !validateEmail() || !validateMessage()){
+        submitError.style.display= 'block';
+        submitError.innerHTML = 'please fix error to submit';
+        setTimeout(function(){submitError.style.display= 'none';}, 3000);
+        return false;
+    }
+}
