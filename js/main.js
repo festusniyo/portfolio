e146ca69c07ed111ff3589ad8fb5735f2925b5c4
festusniyo <84879082+festusAlb@users.noreply.github.com>
2023-01-07 11:21:01 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -68,6 +68,22 @@
       backDelay: 2000
     });
   }
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
  
 
 })()
